f4e4679176c97e4d9d5b47bfe750802dcef481bf
AndrewSt_FR <andrej_stelmax@bk.ru>
2021-09-04 23:02:13 +0700

    CrudController tests have been created

--- MusicService/MusicService.Tests/CrudControllerTests.cs
+++ MusicService/MusicService.Tests/CrudControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using MusicService.Controllers;
 using MusicService.Entity;
 using MusicService.Services;
@@ -14,21 +15,38 @@ namespace MusicService.Tests
     [TestFixture]
     class CrudController
     {
-        public object Time { get; private set; }
+        private Guid _id = Guid.NewGuid();
+
+        private List<EntityBaseDto> GetEntityList(bool isDeleted = false)
+        { 
+            var entities = new List<EntityBaseDto>()
+            {
+                new EntityBaseDto(new EntityBase() { EntityId = Guid.NewGuid()}),
+                new EntityBaseDto(new EntityBase() { EntityId = Guid.NewGuid()}),
+                new EntityBaseDto(new EntityBase() { EntityId = Guid.NewGuid()}),
+                new EntityBaseDto(new EntityBase() { EntityId = Guid.NewGuid()})
+            };
+
+            if (isDeleted == false)
+            {
+                return entities;
+            }
+            else
+            {
+                entities.ForEach(c => c.IsDeleted = true);
+                return entities;
+            }
+        }
+
+        
 
         [Test]
         public async Task GetAll_Void_EntitiesList()
         {
            
             var mockStorage = new Mock<IStorage<EntityBaseDto>>();
-            
-            var expected = new List<EntityBaseDto>()
-            { 
-                new EntityBaseDto(new EntityBase() { EntityId = Guid.NewGuid()}),
-                new EntityBaseDto(new EntityBase() { EntityId = Guid.NewGuid()}),
-                new EntityBaseDto(new EntityBase() { EntityId = Guid.NewGuid()}),
-                new EntityBaseDto(new EntityBase() { EntityId = Guid.NewGuid()})
-            };
+
+            var expected = GetEntityList(true);
 
             var entityBaseList = expected;
 
@@ -39,10 +57,100 @@ namespace MusicService.Tests
 
             mockStorage.Verify(c => c.GetAllEntitiesDto(), Times.Once);
 
-            //Assert.Multiple(() =>
-            //{
-            //    Assert.AreEqual(expected[0].IsDeleted, actual.Value[0].IsDeleted);
-            //});
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expected[0].IsDeleted, actual.Value[0].IsDeleted);
+            });
+
+        }
+
+        [Test]
+        public async Task GetAllDeletedEntities_Void_DeletedEntitiesList()
+        {
+            var mockStorage = new Mock<IStorage<EntityBaseDto>>();
+
+            var expected = GetEntityList();
+
+            var entityBaseDtoList = expected;
+
+            var crudController = new CrudControllerBase<EntityBaseDto>(mockStorage.Object);
+            mockStorage.Setup(c => c.GetAllDeletedEntitiesDto()).ReturnsAsync(entityBaseDtoList);
+
+            var actual = await crudController.GetAllDeleted();
+
+            mockStorage.Verify(c => c.GetAllDeletedEntitiesDto(), Times.Once);
+
+            Assert.Multiple(() =>
+            {
+                for(int i = 0; i < expected.Count; i++)
+                {
+                    Assert.AreEqual(expected[i].IsDeleted, actual.Value[i].IsDeleted);
+                }
+            });
+        }
+
+
+        [Test]
+        public async Task Get_Id_Entity()
+        {
+            var mockStorage = new Mock<IStorage<EntityBaseDto>>();
+
+            var entityBase = new EntityBase();
+            var entityBaseDto = new EntityBaseDto(entityBase);
+            var expected = entityBaseDto;
+
+
+            var crudController = new CrudControllerBase<EntityBaseDto>(mockStorage.Object);
+            mockStorage.Setup(c => c.GetEntityDto(It.IsAny<Guid>())).ReturnsAsync(entityBaseDto);
+
+            var actual = await crudController.Get(_id);
+
+            mockStorage.Verify(c => c.GetEntityDto(It.IsAny<Guid>()), Times.Once);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expected.IsDeleted, actual.Value.IsDeleted);
+            });
+
+        }
+
+        [Test]
+        public async Task Delete_Id_CodeResponse()
+        {
+            var mockStorage = new Mock<IStorage<EntityBaseDto>>();
+            var expected = new OkResult();
+
+            var crudController = new CrudControllerBase<EntityBaseDto>(mockStorage.Object);
+            mockStorage.Setup(c => c.DeleteEntity(It.IsAny<Guid>())).ReturnsAsync(true);
+
+            var actual = await crudController.Delete(_id) as OkResult;
+
+            mockStorage.Verify(c => c.DeleteEntity(It.IsAny<Guid>()), Times.Once);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expected.StatusCode, actual.StatusCode);
+            });
+
+        }
+
+        [Test]
+        public async Task Restore_Id_CodeResponse()
+        {
+            var mockStorage = new Mock<IStorage<EntityBaseDto>>();
+            var expected = new OkResult();
+
+            var crudController = new CrudControllerBase<EntityBaseDto>(mockStorage.Object);
+            mockStorage.Setup(c => c.RestoreEntity(It.IsAny<Guid>())).ReturnsAsync(true);
+
+            var actual = await crudController.Restore(_id) as OkResult;
+
+            mockStorage.Verify(c => c.RestoreEntity(It.IsAny<Guid>()), Times.Once);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expected.StatusCode, actual.StatusCode);
+            });
 
         }
     }
